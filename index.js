5a3dcb4f7db191375286fec7cd06070c1501419e
Zahid <zahid67635gmail.com>
2022-11-10 18:37:28 +0600

    deleted an api

--- index.js
+++ index.js
@@ -78,11 +78,16 @@ async function run() {
         })
 
         app.get('/review/:id', async (req, res) => {
-            const id = req.params.id;
             const query = {};
             const cursor = reviewCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
+        })
+        app.delete('/review/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result)
         })
         //jwt token
 
@@ -111,6 +116,7 @@ async function run() {
             const orders = await cursor.toArray();
             res.send(orders);
         });
+
     }
     finally {
 
